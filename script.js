18a8206e69cbed93ccfd4de31d38ce0dd5c158d4
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-08-25 19:42:07 +0200

    document object module

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("li a").style.color = `red`;
-document.querySelector("button").style.backgroundColor = `yellow`;
-
-function toggleF() {
-    document.querySelectorAll("li")[0].classList.toggle("huge");    
-}
